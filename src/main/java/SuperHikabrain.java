0e72b7d7ab2743401af1ee00048e5754201c68b0
samyeuh <samybouhamidi2@gmail.com>
2024-12-24 17:47:17 +0100

    init

---
+++ src/main/java/SuperHikabrain.java
@@ -0,0 +1,10 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SuperHikabrain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+
+}
